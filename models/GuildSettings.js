15dd7ebb6e2287938177db56f1de4d1a6e65e1b6
Cwkhan <65243374+Khanmanan@users.noreply.github.com>
2025-04-22 21:52:40 +0530

    Update GuildSettings.js

--- models/GuildSettings.js
+++ models/GuildSettings.js
@@ -3,6 +3,9 @@ const { Schema, model } = require('mongoose');
 const settingsSchema = new Schema({
   guildId: String,
   automodEnabled: { type: Boolean, default: false },
+  antiLink: { type: Boolean, default: false },
+  antiSpam: { type: Boolean, default: false },
+  antiGhostPing: { type: Boolean, default: false },
 });
 
 module.exports = model('GuildSettings', settingsSchema);
