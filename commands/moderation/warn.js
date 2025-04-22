42cbd08f523044f6445e1fad3e03340ba84b16f5
Cwkhan <65243374+Khanmanan@users.noreply.github.com>
2025-04-22 22:15:31 +0530

    Update warn.js

--- commands/moderation/warn.js
+++ commands/moderation/warn.js
@@ -5,40 +5,40 @@ const logToChannel = require('../../utils/logToChannel');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('warn')
-    .setDescription('Warn a member')
-    .addUserOption(option =>
-      option.setName('user').setDescription('The user to warn').setRequired(true))
-    .addStringOption(option =>
-      option.setName('reason').setDescription('Reason for warning').setRequired(true))
-    .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers),
+    .setDescription('Warn a user')
+    .addUserOption(option => option.setName('user').setDescription('User to warn').setRequired(true))
+    .addStringOption(option => option.setName('reason').setDescription('Reason for the warning').setRequired(true))
+    .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
 
   async execute(interaction) {
     const user = interaction.options.getUser('user');
     const reason = interaction.options.getString('reason');
 
-    if (user.bot) {
-      return interaction.reply({ content: 'You cannot warn bots.', ephemeral: true });
-    }
-
     let warningData = await Warn.findOne({ userId: user.id, guildId: interaction.guild.id });
     if (!warningData) {
-      warningData = new Warn({ userId: user.id, guildId: interaction.guild.id, warnings: [] });
+      warningData = new Warn({
+        userId: user.id,
+        guildId: interaction.guild.id,
+        warnings: []
+      });
     }
 
-    warningData.warnings.push({ modId: interaction.user.id, reason });
+    warningData.warnings.push({
+      modId: interaction.user.id,
+      reason: reason,
+      date: new Date()
+    });
+
     await warningData.save();
 
     const embed = new EmbedBuilder()
-      .setTitle('⚠️ Warn Issued')
-      .setDescription(`Successfully warned ${user.tag}`)
-      .addFields({ name: 'Reason', value: reason })
-      .setColor('Yellow')
-      .setFooter({ text: `Moderator: ${interaction.user.tag}` })
+      .setTitle('⚠️ Member Warned')
+      .setDescription(`Warned ${user.tag} for: **${reason}**`)
+      .setColor('Orange')
       .setTimestamp();
 
     await interaction.reply({ embeds: [embed] });
 
-    // Log to modlog channel
     await logToChannel(interaction.guild, {
       title: '⚠️ User Warned',
       fields: [
@@ -46,7 +46,7 @@ module.exports = {
         { name: 'Moderator', value: `<@${interaction.user.id}>`, inline: true },
         { name: 'Reason', value: reason }
       ],
-      color: 'Yellow'
+      color: 'Orange'
     });
   }
 };
