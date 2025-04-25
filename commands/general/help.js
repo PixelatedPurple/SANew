13c9c2af562fc5bb6cf170f4e4f99daa3bde1f77
Cwkhan <65243374+Khanmanan@users.noreply.github.com>
2025-04-25 21:59:11 +0530

    Update help.js

--- commands/general/help.js
+++ commands/general/help.js
@@ -1,10 +1,10 @@
 const {
   SlashCommandBuilder,
-  EmbedBuilder,
   ActionRowBuilder,
+  StringSelectMenuBuilder,
+  EmbedBuilder,
   ButtonBuilder,
   ButtonStyle,
-  ComponentType
 } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
@@ -12,80 +12,45 @@ const path = require('path');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('help')
-    .setDescription('Displays all commands by category with pagination.'),
+    .setDescription('View all commands organized by category'),
 
-  async execute(interaction, client) {
-    const commandFolders = fs.readdirSync('./commands');
-    const pages = [];
+  async execute(interaction) {
+    const commandPath = path.join(__dirname, '../');
+    const categories = fs.readdirSync(commandPath).filter(folder => fs.statSync(path.join(commandPath, folder)).isDirectory());
 
-    for (const folder of commandFolders) {
-      const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
-      const commands = [];
+    const menu = new StringSelectMenuBuilder()
+      .setCustomId('help-menu')
+      .setPlaceholder('Select a category')
+      .addOptions(
+        categories.map(cat => ({
+          label: cat.charAt(0).toUpperCase() + cat.slice(1),
+          value: cat,
+          description: `Commands under ${cat}`,
+        }))
+      );
 
-      for (const file of commandFiles) {
-        try {
-          const command = require(`../../commands/${folder}/${file}`);
-          if (command?.data?.name) {
-            commands.push(`• \`/${command.data.name}\` - ${command.data.description || 'No description'}`);
-          }
-        } catch (err) {
-          console.warn(`❌ Skipping invalid command: ${folder}/${file}`, err.message);
-        }
-      }
+    const row = new ActionRowBuilder().addComponents(menu);
 
-      pages.push({
-        name: folder.charAt(0).toUpperCase() + folder.slice(1),
-        value: commands.length ? commands.join('\n') : '_No commands found in this category._'
-      });
-    }
-
-    let currentPage = 0;
-
-    const generateEmbed = (page) => {
-      const embed = new EmbedBuilder()
-        .setTitle(`📖 Help Menu - ${pages[page].name}`)
-        .setDescription(pages[page].value)
-        .setColor('Blurple')
-        .setFooter({ text: `Page ${page + 1} of ${pages.length}` });
-
-      return embed;
-    };
-
-    const row = new ActionRowBuilder().addComponents(
-      new ButtonBuilder().setCustomId('prev').setLabel('⬅️ Prev').setStyle(ButtonStyle.Secondary),
-      new ButtonBuilder().setCustomId('next').setLabel('Next ➡️').setStyle(ButtonStyle.Secondary),
+    const buttons = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setLabel('Support Server')
+        .setStyle(ButtonStyle.Link)
+        .setURL('https://cwkbot.fun/discord'),
+      new ButtonBuilder()
+        .setLabel('Dashboard')
+        .setStyle(ButtonStyle.Link)
+        .setURL('https://bot.cwkbot.fun')
     );
 
-    const msg = await interaction.reply({
-      embeds: [generateEmbed(currentPage)],
-      components: [row],
-      ephemeral: true
-    });
-
-    const collector = msg.createMessageComponentCollector({
-      componentType: ComponentType.Button,
-      time: 60000
-    });
-
-    collector.on('collect', async i => {
-      if (i.user.id !== interaction.user.id) {
-        return i.reply({ content: 'Only you can use these buttons.', ephemeral: true });
-      }
-
-      if (i.customId === 'next') {
-        if (currentPage < pages.length - 1) currentPage++;
-      } else if (i.customId === 'prev') {
-        if (currentPage > 0) currentPage--;
-      }
-
-      await i.update({
-        embeds: [generateEmbed(currentPage)],
-        components: [row]
+    const embed = new EmbedBuilder()
+      .setTitle('📘 Help Menu')
+      .setDescription('Use the dropdown below to explore commands by category.')
+      .setColor('#2f3136')
+      .setFooter({
+        text: 'Automod Bot - Your moderation partner',
+        iconURL: interaction.client.user.displayAvatarURL(),
       });
-    });
 
-    collector.on('end', async () => {
-      await msg.edit({ components: [] });
-    });
-  }
+    await interaction.reply({ embeds: [embed], components: [row, buttons], ephemeral: true });
+  },
 };
